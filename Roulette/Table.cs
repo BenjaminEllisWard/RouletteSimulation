167ae45d7661b0e4caa7c063ec15dcb784b9a9fb
Ben Ward <benjamin.e.ward@outlook.com>
2018-09-29 22:08:24 -0400

    Added double row bet

--- Roulette/Table.cs
+++ Roulette/Table.cs
@@ -25,28 +25,7 @@ namespace Roulette
         public void EvenOrOdd(int number)
         {
             string message = "Even/Odd: ";
-            if (number % 2 == 0)
-            {
-                for (int i = 0; i <= 35; i++)
-                {
-                    if (Numbers[i] % 2 == 0)
-                    {
-                       message += $"{Numbers[i]}, ";
-                    }
-                }
-            }
-
-            if (number % 2 != 0)
-            {
-                for (int i = 0; i <= 35; i++)
-                {
-                    if (Numbers[i] % 2 != 0)
-                    {
-                        message += $"{Numbers[i]}, ";
-                    }
-                }
-            }
-            message = message.Substring(0, message.Length -2);
+            message += (number % 2 == 0) ? "Even" : "Odd";
             Console.WriteLine(message);
         }
 
@@ -64,21 +43,7 @@ namespace Roulette
                 }
             }
 
-            if (isRed == false)
-            {
-                for (int i = 0; i <= Black.Length - 1; i++)
-                {
-                    message += $"{Black[i]}, ";
-                }
-            }
-            else
-            {
-                for (int i = 0; i <= Red.Length - 1; i++)
-                {
-                    message += $"{Red[i]}, ";
-                }
-            }
-            message = message.Substring(0, message.Length - 2);
+            message += (isRed == true) ? "Red" : "Black";
             Console.WriteLine(message);
         }
 
@@ -87,14 +52,7 @@ namespace Roulette
             string message = "Low/High: ";
 
             int condition = (number - 1) / 18;
-            for (int i = 0; i <= 35; i++)
-            {
-                if ((Numbers[i] - 1) / 18 == condition)
-                {
-                    message += $"{Numbers[i]}, ";
-                }
-            }
-            message = message.Substring(0, message.Length - 2);
+            message += (condition < 1) ? "1 to 18" : "19 to 36";
             Console.WriteLine(message);
         }
 
@@ -103,14 +61,18 @@ namespace Roulette
             string message = "Dozens: ";
 
             int condition = (number - 1) / 12;
-            for (int i = 0; i <= 35; i++)
+            if (condition == 0)
             {
-                if ((Numbers[i] - 1) / 12 == condition)
-                {
-                    message += $"{Numbers[i]}, ";
-                }
+                message += "1 to 12";
             }
-            message = message.Substring(0, message.Length - 2);
+            else if (condition == 1)
+            {
+                message += "13 to 24";
+            }
+            else
+            {
+                message += "25 to 36";
+            }
             Console.WriteLine(message);
         }
 
@@ -119,14 +81,18 @@ namespace Roulette
             string message = "Column: ";
 
             int column = number % 3;
-            for (int i = 0; i <= 35; i++)
+            if (column == 1)
             {
-                if (Numbers[i] % 3 == column)
-                {
-                    message += $"{Numbers[i]}, ";
-                }
+                message += "1";
             }
-            message = message.Substring(0, message.Length - 2);
+            else if (column == 2)
+            {
+                message += "2";
+            }
+            else
+            {
+                message += "3";
+            }
             Console.WriteLine(message);
         }
 
@@ -139,11 +105,62 @@ namespace Roulette
             {
                 if ((Numbers[i] - 1) / 3 == street)
                 {
-                    message += $"{Numbers[i]}, ";
+                    message += $"{Numbers[i]}/";
                 }
             }
-            message = message.Substring(0, message.Length - 2);
+            message = message.Substring(0, message.Length - 1);
             Console.WriteLine(message);
+        }
+
+        public void DoubleStreet(int number)
+        {
+            string message = "Double Rows: ";
+            string resultMessage = "";
+
+            int street = (number - 1) / 3;
+            if (number > 3)
+            {
+                for (int i = 0; i <= 35; i++)
+                {
+                    if ((Numbers[i] + 2) / 3 == street)
+                    {
+                        resultMessage += $"{Numbers[i]}/";
+                    }
+                }
+                for (int i = 0; i <= 35; i++)
+                {
+                    if ((Numbers[i] - 1) / 3 == street)
+                    {
+                        resultMessage += $"{Numbers[i]}/";
+                    }
+                }
+                resultMessage = resultMessage.Substring(0, resultMessage.Length - 1);
+            }
+            if (number < 34)
+            {
+                resultMessage += ", ";
+                for (int i = 0; i <= 35; i++)
+                {
+                    if ((Numbers[i] - 1) / 3 == street)
+                    {
+                        resultMessage += $"{Numbers[i]}/";
+                    }
+                }
+                for (int i = 0; i <= 35; i++)
+                {
+                    // The second condition was infuriating to figure out. Turns out that negative division
+                    // truncates toward zero, doesn't "round down." Second condition is necessary so that numbers
+                    // below 4 behave correctly (-1 / 0 would still == street).
+                    if ((Numbers[i] - 4) / 3 == street && (Numbers[i] - 3 > 0))
+                    {
+                        resultMessage += $"{Numbers[i]}/";
+                    }
+                }
+                resultMessage = resultMessage.Substring(0, resultMessage.Length - 1);
+            }
+
+            resultMessage = (number < 4) ? resultMessage.Substring(2, resultMessage.Length - 2) : resultMessage;
+            Console.WriteLine(message + resultMessage);
         }
 
         public void Split (int number)
@@ -244,7 +261,10 @@ namespace Roulette
         {
             Random rnd = new Random();
             int number = rnd.Next(1, 38);
-            number = 36;
+
+            // use the statement below to test a specific number
+            // 0 and 00 correspond to number = 37, 38
+            //number = 38;
 
             
 
@@ -252,6 +272,7 @@ namespace Roulette
             {
                 Console.WriteLine($"Winning number: {number}");
                 Console.WriteLine();
+                Console.WriteLine("Winning bets:");
                 Assign();
                 EvenOrOdd(number);
                 RedOrBlack(number);
@@ -259,8 +280,10 @@ namespace Roulette
                 Dozens(number);
                 Column(number);
                 Street(number);
+                DoubleStreet(number);
                 Split(number);
                 Corner(number);
+                Console.WriteLine();
             }
             else if (number == 37)
             {
